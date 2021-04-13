4792625fc7e4a062e63241deac7d5bb4f1bff8dc
rebozasambirejo <cah.sambirejo47@gmail.com>
2021-04-13 10:18:30 +0700

    Update and rename copy.js to parse-html.js

--- parse-html.js
+++ parse-html.js
@@ -1,3 +1,32 @@
+function cdClear() {
+    var wtarea = document.getElementById('codes');
+    wtarea.value = '';
+    wtarea.focus();
+    document.getElementById('convert').disabled = false;
+  document.getElementById("btn-copy")
+        .style.display = "none"
+}
+function cdConvert() {
+    var ctarea = document.getElementById('codes'),
+        cv = ctarea.value,
+        opt1 = document.getElementById('opt1'),
+        opt2 = document.getElementById('opt2'),
+        opt3 = document.getElementById('opt3'),
+        opt4 = document.getElementById('opt4'),
+        opt5 = document.getElementById('opt5');
+    cv = cv.replace(/t/g, "    ");
+    if (opt1.checked) cv = cv.replace(/&/g, "&amp;");
+    if (opt2.checked) cv = cv.replace(/</g, "&lt;");
+    if (opt3.checked) cv = cv.replace(/>/g, "&gt;");
+    if (opt4.checked) cv = cv.replace(/"/g, "&quot;");
+    if (opt5.checked) cv = cv.replace(/'/g, "&#039;");
+    ctarea.value = cv;
+    ctarea.focus();
+    ctarea.select();
+  	document.getElementById("btn-copy")
+        .style.display = "inline-block";
+};
+
 /*!
  * clipboard.js v1.5.16
  * https://zenorocha.github.io/clipboard.js
